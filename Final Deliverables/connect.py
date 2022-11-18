c4de83f3deba617abb72c59a7b082691d4176fca
karthirohini <67254594+karthirohini@users.noreply.github.com>
2022-11-18 12:30:55 +0530

    Add files via upload

---
+++ Final Deliverables/connect.py
@@ -0,0 +1,77 @@
+import ibm_db
+import sqlite3
+dsn_hostname = "***********************.databases.appdomain.cloud" # e.g.: "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "user id"        # e.g. "abc12345"
+dsn_pwd = "password"      # e.g. "7dBZ3wWt9XN6$o0J"
+dsn_driver = "{IBMDB2CL1}"
+dsn_database = "bludb"            # e.g. "BLUDB"
+dsn_port = "32459"                # e.g. "32733"
+dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_security = "SSL"              #i.e. "SSL
+dsn_cert="DigiCertGlobalRootCA.crt"
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};"
+    "SSLServerCertificate={8};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security,dsn_cert)
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+except:
+    print("Unable to connect: ", ibm_db.conn_errormsg())
+# sql = "insert into user values('105','sahana@gmail.com','1234','sahanaparveen')"
+# ibm_db.exec_immediate(conn, sql)
+print(dsn)
+
+
+#Open database
+conn = sqlite3.connect('database.db')
+
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password TEXT,
+		email TEXT,
+		firstName TEXT,
+		lastName TEXT,
+		address1 TEXT,
+		address2 TEXT,
+		zipcode TEXT,
+		city TEXT,
+		state TEXT,
+		country TEXT, 
+		phone TEXT
+		)''')
+
+conn.execute('''CREATE TABLE products
+		(productId INTEGER PRIMARY KEY,
+		name TEXT,
+		price REAL,
+		description TEXT,
+		image TEXT,
+		stock INTEGER,
+		categoryId INTEGER,
+		FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
+		)''')
+
+conn.execute('''CREATE TABLE kart
+		(userId INTEGER,
+		productId INTEGER,
+		FOREIGN KEY(userId) REFERENCES users(userId),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
+
+conn.execute('''CREATE TABLE categories
+		(categoryId INTEGER PRIMARY KEY,
+		name TEXT
+		)''')
+
+
+
+conn.close()
+
